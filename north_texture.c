2fc30e4dfd8fb90237855278aeb4bbee865b99c7
Sylvain Ducamp <sylducam@z4r2p3.42lyon.fr>
2021-04-14 14:11:56 +0200

    mon test avec un directory.xpm etait mal ecrit donc a retester. les fichiers de textures doivent etre stockes en utilisant le path entier, pas un fd. Pauline a fini son parsing de textures apparemment donc je push pour ensuite recuperer ce qu'elle a fait. Il lui manque juste a gere ce probleme d'open directory qui aurait le format .xpm ou .png

--- north_texture.c
+++ north_texture.c
@@ -6,7 +6,7 @@
 /*   By: sylducam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 16:02:15 by sylducam          #+#    #+#             */
-/*   Updated: 2021/04/14 12:47:58 by sylducam         ###   ########lyon.fr   */
+/*   Updated: 2021/04/14 14:02:09 by sylducam         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,52 +18,37 @@
 #include "minilibx/mlx.h" // a virer
 
 /*
+ * la fonction principale :
  *
+ * verifie d'abord que l'identifiant n'a pas deja ete rencontre avec le booleen settings->NO
+ * split la line avec espace comme separateur
+ * elle appelle right_amount qui verifie qu'il y a bien 2 elements (l'identifiant et le path)
  *
+ * celle-ci appelle right_file qui verifie - que l'idenfifiant est bien "NO"
+ * 										   - que le format du fichier de texture est bien .xpm ou .png via la fonction format_check qui est dans ma libft
+ * 										   - le path et que le fichier soit ouvrable, ces deux choses en utilisant open
  *
+ *  
  *
- *
- *
- *
+ * a chaque fois s'il y a une erreur, 1 est renvoye, 0 si tout se passe bien
+ * si tout s'est bien passe, settings->NO est mis a true
 */
-
-
-// tu es en train de faire une serie de tests et particulierement, conrpendre les retours d'open car il y a un probleme avec le test 8 je crois
-// testes avec un directory.xpm ou .png Pauline dit qu'il faut faire deux open pour regler ce souci
 
 typedef struct	s_settings
 {
 	bool	NO;
 	int		north_fd;
-	void	*mlx; // si ca marche, a bien ajouter a ta structure dans le header
+	void	*mlx;
 }				cub_settings;
-
-/*static int	right_format(char *file, char *format)
-{
-	if (file && format && *file && *format)
-	{
-		while (*file)
-			file++;
-		file -= strlen(format);
-		while (*file && *format)
-		{
-			if (*file != *format)
-				return (1);
-			file++;
-			format++;
-		}
-	}
-	return (0);
-}*/
 
 static int	right_file(char **elements, cub_settings *settings)
 {
-	if (ft_strcmp(elements[0], "NO")) // ok niveau parentheses ? trop ?
+	if (ft_strcmp(elements[0], "NO"))
 		return (1);
 	if (((format_check(elements[1], ".xpm")) +
 			(format_check(elements[1], ".png"))) == 2) // formulation un peu bizarre mais ca ne marchait pas avec && ou juste +. 2 veut dire que les deux formats ne correspondent pas (2 retours d'erreur). Si c'est 1 c'est ok car l'un deux match.
 		return (1);
-	if ((settings->north_fd = open(elements[1], O_RDONLY)) < 1)
+	if ((settings->north_fd = open(elements[1], O_RDONLY)) < 0)
 		return (1);
 	return (0);
 }
@@ -94,64 +79,4 @@ int			north_texture(char *line, cub_settings *settings)
 	settings->NO = true;
 	free(elements);
 	return (0);
-}
-
-int main()
-{
-//    void    *mlx; // a ajouter dans ton main a toi
-
-//    mlx = mlx_init(); // a ajouter dans ton main a toi
-	int ret = 0;
-//	char *line = "NO    ./textures/grass.xpm    ./textures/stone.png    ";
-	cub_settings settings;
-//	settings.NO = false;
-	settings.mlx = mlx_init();
-/*	dprintf(1, "--- test 1 ---\n");
-	ft_bzero(&settings, sizeof(cub_settings));
-	ret = north_texture("N     ./././     ", &settings);
-	dprintf(1, "ret = %d\n", ret);
-	dprintf(1, "NO = %d\n", settings.NO);
-	dprintf(1, "north_texture_fd = %d\n", settings.north_fd);
-	dprintf(1, "--- test 2 ---\n");
-	ft_bzero(&settings, sizeof(cub_settings));
-	ret = north_texture("NO    textures/grass.xpm    ", &settings);
-	dprintf(1, "ret = %d\n", ret);
-	dprintf(1, "NO = %d\n", settings.NO);
-	dprintf(1, "north_texture_fd = %d\n", settings.north_fd);
-	dprintf(1, "--- test 3 ---\n");
-	ft_bzero(&settings, sizeof(cub_settings));
-	ret = north_texture("NO    38     xpm    ", &settings);
-	dprintf(1, "ret = %d\n", ret);
-	dprintf(1, "NO = %d\n", settings.NO);
-	dprintf(1, "north_texture_fd = %d\n", settings.north_fd);
-	dprintf(1, "--- test 4 ---\n");
-	ft_bzero(&settings, sizeof(cub_settings));
-	ret = north_texture("NO   38   textures/stone.png   ", &settings);
-	dprintf(1, "ret = %d\n", ret);
-	dprintf(1, "NO = %d\n", settings.NO);
-	dprintf(1, "north_texture_fd = %d\n", settings.north_fd);
-	dprintf(1, "--- test 5 ---\n");
-	ft_bzero(&settings, sizeof(cub_settings));
-	ret = north_texture("NO   ./textures/stone.png   ./textures/grass.xpm    ", &settings);
-	dprintf(1, "ret = %d\n", ret);
-	dprintf(1, "NO = %d\n", settings.NO);
-	dprintf(1, "north_texture_fd = %d\n", settings.north_fd);
-	dprintf(1, "--- test 6 ---\n");
-	ft_bzero(&settings, sizeof(cub_settings));
-	ret = north_texture("NO    ./textures/file.txt    ", &settings);
-	dprintf(1, "ret = %d\n", ret);
-	dprintf(1, "NO = %d\n", settings.NO);
-	dprintf(1, "north_texture_fd = %d\n", settings.north_fd);*/
-	dprintf(1, "--- test 7 ---\n");
-	ft_bzero(&settings, sizeof(cub_settings));
-	ret = north_texture("NO    ./textures./grass.xpm    ", &settings);
-	dprintf(1, "ret = %d\n", ret);
-	dprintf(1, "NO = %d\n", settings.NO);
-	dprintf(1, "north_texture_fd = %d\n", settings.north_fd);
-/*	dprintf(1, "--- test 8 ---\n");
-	ft_bzero(&settings, sizeof(cub_settings));
-	ret = north_texture("NOrdine    ./textures./grass.xpm    ", &settings);
-	dprintf(1, "ret = %d\n", ret);
-	dprintf(1, "NO = %d\n", settings.NO);
-	dprintf(1, "north_texture_fd = %d\n", settings.north_fd);*/
 }
