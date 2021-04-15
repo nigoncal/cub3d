345133c537e3c33098001a77e2ae8dc7700928bd
Sylvain Ducamp <sylducam@z4r2p3.42lyon.fr>
2021-04-15 13:03:04 +0200

    format_check.c corrige dans ma libft

--- libft/format_check.c
+++ libft/format_check.c
@@ -6,7 +6,7 @@
 /*   By: sylducam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 12:57:13 by sylducam          #+#    #+#             */
-/*   Updated: 2021/04/13 18:36:25 by sylducam         ###   ########lyon.fr   */
+/*   Updated: 2021/04/15 13:02:12 by sylducam         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,17 @@ int	format_check(char *file, char *format)
 
 	i = 0;
 	j = 0;
-	if (file && format && *file && *format)
+	if (!file || !format || !*file || !*format)
+		return (1);
+	while (file[i])
+		i++;
+	i -= strlen(format);
+	while (file[i] && format[j])
 	{
-		while (file[i])
-			i++;
-		i -= strlen(format);
-		while (file[i] && format[j])
-		{
-			if (file[i] != format[j])
-				return (1);
-			i++;
-			j++;
-		}
+		if (file[i] != format[j])
+			return (1);
+		i++;
+		j++;
 	}
 	return (0);
 }
