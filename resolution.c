c0ac094fd4eabd5aca8f11aef010adafe35ee555
Sylvain Ducamp <sylducam@z4r2p3.42lyon.fr>
2021-04-16 13:07:36 +0200

    resolution normalement fini de modifie avec les if qui n'incluent plus des fonctions et too_big_for_screen corrigee sans pointeurs pour les int et sans mallocs. a tester tout ca

--- resolution.c
+++ resolution.c
@@ -6,7 +6,7 @@
 /*   By: sylducam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 15:11:38 by sylducam          #+#    #+#             */
-/*   Updated: 2021/04/15 11:34:09 by sylducam         ###   ########lyon.fr   */
+/*   Updated: 2021/04/16 13:01:14 by sylducam         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,59 +50,78 @@ typedef struct	s_settings
 void	too_big_for_screen(t_settings *cub_sets)
 {
 	int trigger;
-	int	*screen_width;
-	int	*screen_height;
+	int	screen_width;
+	int	screen_height;
 	
-	screen_width = malloc(sizeof(int));
-	screen_height = malloc(sizeof(int));
-	trigger = mlx_get_screen_size(cub_sets->mlx, screen_width, screen_height);
-	if (cub_sets->width > *screen_width)
-		cub_sets->width = *screen_width;
-	if (cub_sets->height > *screen_height)
-		cub_sets->height = *screen_height;
-	free(screen_width);
-	free(screen_height);
+	trigger = mlx_get_screen_size(cub_sets->mlx, &screen_width, &screen_height);
+	if (cub_sets->width > screen_width)
+		cub_sets->width = screen_width;
+	if (cub_sets->height > screen_height)
+		cub_sets->height = screen_height;
 }
 
-static int	check_elements(char **elements, t_settings *cub_sets)
+static int	right_content(char **elements, t_settings *cub_sets)
 {
 	int	i;
+	int	error;
 
 	i = 0;
-	while (elements[i])
-		i++;
-	if (i != 3)
-		return (1);
-	i = 0;
-	if (ft_strcmp(elements[0], "R"))
-		return (1);
+	error = 1;
 	while (elements[1][i])
-		if (!(ft_isdigit(elements[1][i++])))
-			return (1);
+	{
+		error = ft_isdigit(elements[1][i++]);
+		if (error == 0)
+			return (-1);
+	}
 	i = 0;
+	error = 1;
 	while (elements[2][i])
-		if (!(ft_isdigit(elements[2][i++])))
-			return (1);
+	{
+		error = ft_isdigit(elements[2][i++]);
+		if (error == 0)
+			return (-1);
+	}
 	cub_sets->width = ft_atoi(elements[1]);
 	cub_sets->height = ft_atoi(elements[2]);
+	return (0);
+}
+
+static int	right_amount(char **elements, t_settings *cub_sets)
+{
+	int	error;
+
+	error = 0;
+	while (elements[error])
+		error++;
+	if (error != 3)
+		return (-1);
+	error = ft_strcmp(elements[0], "R");
+	if (error != 0)
+		return (-1);
+	error = right_content(elements, cub_sets);
+	if (error == -1)
+		return (-1);
 	if (cub_sets->width <= 0 || cub_sets->height <= 0)
-		return (1);
+		return (-1);
 	too_big_for_screen(cub_sets);
-	cub_sets->R = true;
 	return (0);
 }
 
 void	resolution(char *line, t_settings *cub_sets)
 {
-	char **elements;
+	int		error;
+	char	**elements;
 
+	error = 0;
 	if (cub_sets->R == true)
 		dprintf(1, "R already true\n");
 	//	abort_prog(line, cub_sets, "Identifiers should be used only once");
 	elements = ft_split(line, ' ');
-	if (check_elements(elements, cub_sets))
+	error = right_amount(elements, cub_sets);
+	if (error == -1)
 		dprintf(1, "Error\n");
 	//	abort_prog(line, cub_sets, "Resolution line not well formated. Usage :\
 	//			\nR <width> <height>\nOnly positive values");
+	cub_sets->R = true;
 	free(elements);
 }
