a5f5c704e5954f1a759d2d42d66432e08d766e6e
Sylvain Ducamp <sylducam@z3r2p5.42lyon.fr>
2021-04-20 13:33:12 +0200

    free_char_p2p.c absolument ok

--- libft/free_char_p2p.c
+++ libft/free_char_p2p.c
@@ -6,52 +6,29 @@
 /*   By: sylducam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/18 18:16:22 by sylducam          #+#    #+#             */
-/*   Updated: 2021/04/19 16:05:05 by sylducam         ###   ########lyon.fr   */
+/*   Updated: 2021/04/20 13:32:24 by sylducam         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header_libft.h"
 #include <stdlib.h>
 
-
 // This function frees char pointer to pointers
 
-int		free_char_p2p(char **str)
+int	free_char_p2p(char **str)
 {
 	int	i;
 
 	i = 0;
-	dprintf(1, "str[0] = %p\n",  str[0]);
-	dprintf(1, "str[1] = %p\n",  str[1]);
-	dprintf(1, "str[2] = %p\n",  str[2]);
-	dprintf(1, "\n");
 	if (!str || !*str)
 		return (-1);
 	while (str[i])
 	{
 		free(str[i]);
+		str[i] = NULL;
 		i++;
 	}
-	dprintf(1, "str[0] = %p\n",  str[0]);
-	dprintf(1, "str[1] = %p\n",  str[1]);
-	dprintf(1, "str[2] = %p\n",  str[2]);
-	dprintf(1, "str = %p\n",  str);
 	free(str);
-	dprintf(1, "str = %p\n",  str);
+	str = NULL;
 	return (0);
-}
-
-int main()
-{
-	char **str = ft_split("   R   150   166   ", ' ');
-	dprintf(1, "str[0] = %p\n",  str[0]);
-	dprintf(1, "str[1] = %p\n",  str[1]);
-	dprintf(1, "str[2] = %p\n",  str[2]);
-	dprintf(1, "str = %p\n",  str);
-	dprintf(1, "\n");
-	free_char_p2p(str);
-	dprintf(1, "str[0] = %p\n",  str[0]);
-	dprintf(1, "str[1] = %p\n",  str[1]);
-	dprintf(1, "str[2] = %p\n",  str[2]);
-	dprintf(1, "str = %p\n",  str);
 }
