945f1cf61fa1c247d045b219ce74038ab80e3777
Sylvain Ducamp <sylducam@z4r1p5.42lyon.fr>
2021-05-28 11:28:40 +0200

    error_manager.c modifie et les sprites ne sont plus pris en compte dans tout le code

--- srcs/error_manager.c
+++ srcs/error_manager.c
@@ -6,7 +6,7 @@
 /*   By: sylducam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 08:46:12 by sylducam          #+#    #+#             */
-/*   Updated: 2021/05/26 14:44:59 by sylducam         ###   ########lyon.fr   */
+/*   Updated: 2021/05/28 11:25:39 by sylducam         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,24 @@ void	map_error(void)
 {
 	ft_putstr("An error was spotted in your map\nRules of the map :\n");
 	ft_putstr("The only characters allowed are 0 for the floor, ");
-	ft_putstr("1 for the walls, 2 for the sprites, N/S/E/W for the player ");
+	ft_putstr("1 for the walls, N/S/E/W for the player ");
 	ft_putstr("according to it's orientation and spaces.\n");
 	ft_putstr("The map has to be closed by walls and space characters are ");
-	ft_putstr("only allowed outside the map to shape it to your will.\n");
+	ft_putstr("only allowed around the map to shape it to your will.");
+	ft_putstr("Here's an example of a simple valid map :\n\n");
+	ft_putstr("        1111111111111111111111111\n");
+	ft_putstr("        1000000000110000000000001\n");
+	ft_putstr("        1011000001110000000000001\n");
+	ft_putstr("        1001000000000000000000001\n");
+	ft_putstr("111111111011000001110000000000001\n");
+	ft_putstr("100000000011000001110111111111111\n");
+	ft_putstr("11110111111111011100000010001\n");
+	ft_putstr("11110111111111011101010010001\n");
+	ft_putstr("11000000110101011100000010001\n");
+	ft_putstr("10000000000000001101010010001\n");
+	ft_putstr("11000001110101011111011110N0111\n");
+	ft_putstr("11110111 1110101 101111010001\n");
+	ft_putstr("11111111 1111111 111111111111");
 }
 
 void	abort_prog(char *error_msg)
